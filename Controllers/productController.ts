40195e8ba2134c647d57293730fb6f7c57984c9e
iAmHammad261 <hammad2nd@gmail.com>
2026-07-02 11:58:18 +0500

    Feat: added section id of the 53 in the filter

--- Controllers/productController.ts
+++ Controllers/productController.ts
@@ -9,7 +9,7 @@ import { logger } from "../Utils/logger.js";
 export const getAllProducts = async (req: Request, res: Response) => {
   const client = b24.instance;
   try {
-    const filter = { ">ID": 0 };
+    const filter = { SECTION_ID: 53 };
     const products = await client.actions.v2.callList.make({
       method: "crm.product.list",
       params: {
