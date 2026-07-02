e34d5aad7bef374f880221ed7aab2b5b839cc6a2
iAmHammad261 <hammad2nd@gmail.com>
2026-07-02 14:23:26 +0500

    Feat: add updateProduct endpoint

--- routes.ts
+++ routes.ts
@@ -1,5 +1,7 @@
 import { getAllProducts } from "./Controllers/productController.js";
+import { updateProduct } from "./Controllers/updateProductController.js";
 
 export const setupRoutes = (app: any) => {
   app.get("/getAllProducts", getAllProducts);
+  app.patch("/updateProduct", updateProduct);
 };
